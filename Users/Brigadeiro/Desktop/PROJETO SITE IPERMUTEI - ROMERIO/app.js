abc8ee96951e470cca291ea938c78f17a30000ce
Your Name <you@example.com>
2024-10-22 15:43:50 -0300

    Configuração do servidor para servir arquivos estáticos e HTML

--- Users/Brigadeiro/Desktop/PROJETO SITE IPERMUTEI - ROMERIO/app.js
+++ Users/Brigadeiro/Desktop/PROJETO SITE IPERMUTEI - ROMERIO/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servir arquivos estáticos da pasta 'assets'
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Servir arquivos HTML diretamente da pasta 'pages'
+app.use('/pages', express.static(path.join(__dirname, 'pages')));
+
+// Servir o index.html diretamente na rota principal
 app.get('/', (req, res) => {
-  res.send('Olá, Heroku!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
