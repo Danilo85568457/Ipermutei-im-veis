7f3066a40d9f4f29981b74d1ebce2c8217ec7cfa
Your Name <you@example.com>
2024-10-30 13:51:49 -0300

    Alteração-back-end

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 // Configuração do AWS S3
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1'
+  accessKeyId: 'REMOVED', 
+  secretAccessKey: 'REMOVED',
+  region: 'us-east-1',
 });
 
 const s3 = new AWS.S3();
